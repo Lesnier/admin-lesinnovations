c73217d5594b211f22dbe4bf95ac1a00b523965e
Lesnier Gonzalez Lopez <lesniergonzalez@gmail.com>
2026-02-01 22:44:30 -0500

    Fix: Add client_id and private_key_id to Google Sheets config

--- config/google.php
+++ config/google.php
@@ -46,10 +46,11 @@ return [
     */
     'config' => [
         'type' => 'service_account',
-        'project_id' => 'les-innovations-factory-485418', // Defaulting to one found in email usually
+        'project_id' => 'les-innovations-factory-485418',
+        'private_key_id' => 'dd05719590bb4e9f85c3426136c792f5151f6e63',
         'private_key' => str_replace('\\n', "\n", config('services.google_sheets.private_key')),
         'client_email' => config('services.google_sheets.service_account_email'),
-        'client_id' => '',
+        'client_id' => '108073882259484905091',
         'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
         'token_uri' => 'https://oauth2.googleapis.com/token',
         'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
